3a2270aea97540eceb9904d9fcd35966c3a9482a
Angelo Pumar <angelopumar987@gmail.com>
2024-09-16 21:18:14 +0800

    Padding Changes

--- app/index.tsx
+++ app/index.tsx
@@ -125,7 +125,7 @@ export default function Index() {
         }}>
         <View style={styles.centeredView}>
           <View style={[styles.modalView, {borderWidth: 5}]}>
-            <View style={[styles.note, { borderWidth: 0 }]}>
+            <View style={[styles.note, { borderWidth: 0, marginBottom: 0}]}>
               <TextInput
                 style={[styles.input, styles.inputTitle]} 
                 value={singleNote?.title}
